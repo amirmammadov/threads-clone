181116aed5cf6b7f285124f7f788e7d48476769e
amirmammadov <amirmammado@gmail.com>
2023-10-22 17:07:45 +0400

    small change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,7 +7,16 @@ import ThreadCard from "@/components/cards/ThreadCard";
 
 export default async function Home() {
   const user = await currentUser();
-  // if (!user) return null;
+
+  if (!user)
+    return (
+      <>
+        <h1 className="head-text text-left">Home</h1>
+        <section className="mt-9 flex flex-col gap-10">
+          <p className="no-result">No threads found</p>
+        </section>
+      </>
+    );
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo) redirect("/onboarding");
