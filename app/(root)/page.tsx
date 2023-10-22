dcc1f9cfe2d4817918f370dc15b68dfee0d9db7e
amirmammadov <amirmammado@gmail.com>
2023-10-22 17:10:27 +0400

    some texts for home page was added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
       <>
         <h1 className="head-text text-left">Home</h1>
         <section className="mt-9 flex flex-col gap-10">
-          <p className="no-result">No threads found</p>
+          <p className="no-result">Login and join to us!</p>
         </section>
       </>
     );
